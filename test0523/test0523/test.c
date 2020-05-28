e9593e4ca88ba935781a0b222a84181a3711cf08
Straw-b <1609332043@qq.com>
2020-05-28 21:00:24 +0800

    修改代码

--- test0523/test0523/test.c
+++ test0523/test0523/test.c
@@ -4,8 +4,10 @@
 
 
 //讨论题
+//void PrintArray(int* array, int size)
 void PrintArray(int array[], int size)
 {
+	printf("%d\n",sizeof(array) / sizeof(array[0]));
 	for (int i = 0; i < size; ++i)
 	{
 		printf("%d ", array[i]);
@@ -20,10 +22,13 @@ int main()
 	int* p2 = (int*)(array + 1);
 	char* p3 = NULL;
 
-	int a = p1[-2] + *p2 + sizeof(&array) + sizeof(*p3);//4+6+0+1
+	int a = p1[-2] + *p2 + sizeof(&array) + sizeof(*p3);//4+2+4+1//p3是空指针，实则是char
 	//printf("%d\n", *p3);
 	printf("%d\n", a);//11
 
+	//int* p4 = &array;//err
+
+	//在传参期间数组名已经退化为首元素的地址
 	PrintArray(array, sizeof(array) / sizeof(array[0]));
 
 	return 0;
@@ -36,6 +41,8 @@ int main()
 
 //数组相关面试题
 //4. 旋转数组
+
+
 
 
 //5. 数组形式的整数加法
