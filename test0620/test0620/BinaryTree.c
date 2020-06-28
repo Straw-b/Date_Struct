838dadf0c7cccb8e08c58d9f2d71d1d056d884a0
Straw-b <1609332043@qq.com>
2020-06-28 13:59:42 +0800

    二叉树的实现

---
+++ test0620/test0620/BinaryTree.c
@@ -0,0 +1,325 @@
+#include "BinaryTree.h"
+#include <malloc.h>
+#include <assert.h>
+#include <stdio.h>
+
+#include "Queue.h"
+
+
+BTNode* BuyBinTreeNode(BTNDataType data)
+{
+	BTNode* newNode = (BTNode*)malloc(sizeof(BTNode));
+	if (NULL == newNode)
+	{
+		assert(0);
+		return NULL;
+	}
+
+	newNode->data = data;
+	newNode->left = NULL;
+	newNode->right = NULL;
+	return newNode;
+}
+
+
+// 创建
+// array数组中：保存的是二叉树中所有节点的值域
+// index：利用index取到数组中的每个元素，然后在递归函数中需要对index进行改变，递归结束后需要将改变带出来
+BTNode* CreateBinTree(int array[], int size, int* index, int invaild)
+{
+	BTNode* root = NULL;
+	if (*index < size && array[*index] != -1)
+	{
+		//创建根节点
+		root = BuyBinTreeNode(array[*index]);
+
+		//创建根节点的左子树
+		++(*index);
+		root->left = CreateBinTree(array, size, index, invaild);
+
+		//创建根节点的右子树
+		++(*index);
+		root->right = CreateBinTree(array, size, index, invaild);
+	}
+	return root;
+}
+
+// 二叉树的拷贝
+BTNode* CopyBinTree(BTNode* root)
+{
+	BTNode* newroot = NULL;
+	if (root)
+	{
+		//拷贝根节点
+		newroot = BuyBinTreeNode(root->data);
+
+		//拷贝根节点的左子树
+		newroot->left = CopyBinTree(root->left);
+
+		//拷贝根节点的右子树
+		newroot->right = CopyBinTree(root->right);
+	}
+	return newroot;
+}
+
+
+//假设：遍历操作就是将节点中的值域打印出来
+//根节点--->根节点的左子树--->根节点的右子树
+void PreOrder(BTNode* root)
+{
+	if (root)
+	{
+		printf("%d ", root->data);
+		PreOrder(root->left);
+		PreOrder(root->right);
+	}
+}
+
+void InOrder(BTNode* root)
+{
+	if (root)
+	{
+		InOrder(root->left);
+		printf("%d ", root->data);
+		InOrder(root->right);
+	}
+}
+
+void PostOrder(BTNode* root)
+{
+	if (root)
+	{
+		PostOrder(root->left);
+		PostOrder(root->right);
+		printf("%d ", root->data);
+	}
+}
+
+//层序遍历
+void LevelOrder(BTNode* root)
+{
+	Queue q;
+	if (NULL == root)
+		return;
+
+	QueueInit(&q);
+	QueuePush(&q, root);
+
+	while (!QueueEmpty(&q))
+	{
+		BTNode* cur = QueueFront(&q);
+		printf("%d ", cur->data);
+		QueuePop(&q);
+
+		//如果cur左孩子存在，将左孩子入队列
+		if (cur->left)
+			QueuePush(&q, cur->left);
+
+		//如果cur有孩子存在，将右孩子入队列
+		if (cur->right)
+			QueuePush(&q, cur->right);
+	}
+
+	QueueDestroy(&q);
+}
+
+// 二叉树节点个数
+int GetNodeCount(BTNode* root)
+{
+	//if (NULL == root)
+	//	return 0;
+	//int leftSize = GetNodeCount(root->left);
+	//int rightSize = GetNodeCount(root->right);
+	//return leftSize + rightSize + 1;
+
+	if (NULL == root)
+		return 0;
+	return GetNodeCount(root->left) + GetNodeCount(root->right) + 1;
+
+	//return NULL == root ? 0 : GetNodeCount(root->left) + GetNodeCount(root->right) + 1;
+}
+
+// 二叉树叶子节点个数
+int GetLeafNodeCount(BTNode* root)
+{
+	if (NULL == root)
+		return 0;
+
+	if (NULL == root->left && NULL == root->right)
+		return 1;
+
+	return GetLeafNodeCount(root->left) + GetLeafNodeCount(root->right);
+
+}
+
+// 二叉树第K层节点个数
+int GetKLevelNodeCount(BTNode* root, unsigned int k)
+{
+	if (NULL == root || k == 0)
+		return 0;
+
+	//第一层只有根节点
+	if (k == 1)
+		return 1;
+
+	//将问题转化为：到子树中求k-1层节点的个数
+	return GetKLevelNodeCount(root->left, k - 1) + GetKLevelNodeCount(root->right, k - 1);
+}
+
+// 二叉树的高度
+int GetHight(BTNode* root)
+{
+	int leftHeight = 0, rightHeight = 0;
+	if (NULL == root)
+		return 0;
+
+	leftHeight = GetHight(root->left);
+	rightHeight = GetHight(root->right);
+	return leftHeight > rightHeight ? leftHeight + 1 : rightHeight + 1;
+}
+
+
+// 二叉树查找值为data的节点
+BTNode* Find(BTNode* root, BTNDataType data)
+{
+	BTNode* ret = NULL;
+	if (NULL == root)
+		return NULL;
+
+	if (root->data == data)
+		return root;
+
+	// 先在左子树中找，找到了直接返回
+	// 如果左子树中没有找到，再到右子树中找
+	//(ret = Find(root->left, data)) || (ret = Find(root->right, data));
+	//return ret;
+	
+	if (ret = Find(root->left, data))
+		return ret;
+	return Find(root->right, data);
+}
+
+
+// 判断二叉树是否是完全二叉树
+int BinaryTreeComplete(BTNode* root)
+{
+	Queue q;
+	int flag = 0;
+	int ret = 0;
+	//空数也是完全二叉树
+	if (NULL == root)
+		return 1;
+
+	//非空
+	//1.按照层序遍历规则找第一个不饱和点
+	QueueInit(&q);
+	QueuePush(&q, root);
+	while (!QueueEmpty(&q))
+	{
+		BTNode* cur = QueueFront(&q);
+		if (flag)
+		{
+			//不饱和的节点已经找到
+			//后序节点就不能有孩子
+			if (cur->left || cur->right)
+				break;
+		}
+		else
+		{
+			//找第一个不饱和的点
+			if (cur->left && cur->right)
+			{
+				QueuePush(&q, cur->left);
+				QueuePush(&q, cur->right);
+			}
+			else if (cur->left)
+			{
+				//cur只有左孩子
+				QueuePush(&q, cur->left);
+				flag = 1;
+			}
+			else if (cur->right)
+			{
+				//cur只有右孩子
+				break;
+			}
+			else
+			{
+				flag = 1;
+			}
+		}
+		QueuePop(&q); //队头元素拿出来
+	}
+	
+	if (QueueEmpty(&q))
+		ret = 1;
+
+	QueueDestroy(&q);
+	return ret;
+}
+
+
+//该处的传参与单链表头删是一样的
+void DestroyTree(BTNode** root)
+{
+	assert(root); //root中的内容：就是外部实参的地址
+	if (*root) //*root才是外部的实参
+	{
+		DestroyTree(&(*root)->left);
+		DestroyTree(&(*root)->right);
+		free(*root);
+		*root = NULL; //外部的实参指向空
+	}
+}
+
+
+//////////////////////////////////////////////////////////////////////////
+void TestBinTree()
+{
+	// 将二叉树中元素放在数组中
+	int array[] = { 1, 2, 3, -1, -1, -1, 4, 5, -1, -1, 6 };
+	int index = 0;
+	BTNode* root = CreateBinTree(array, sizeof(array) / sizeof(array[0]), &index, -1);
+	BTNode* newroot = NULL;
+
+	PreOrder(root);
+	printf("\n");
+	InOrder(root);
+	printf("\n");
+	PostOrder(root);
+	printf("\n");
+	LevelOrder(root);
+	printf("\n");
+
+	printf("%d\n", GetNodeCount(root));
+	printf("%d\n", GetLeafNodeCount(root));
+	printf("%d\n", GetKLevelNodeCount(root, 2));
+	printf("%d\n", GetKLevelNodeCount(root, 3));
+	printf("%d\n", GetKLevelNodeCount(root, 10));
+	printf("%d\n", GetHight(root));
+
+	//查找
+	BTNode* cur = Find(root, 5);
+	if (cur)
+		printf("5 is in binary tree!\n");
+	else
+		printf("5 is not in binary tree!\n");
+
+	//是否完全二叉树
+	if (BinaryTreeComplete(root))
+		printf("is complete binary tree!\n");
+	else
+		printf("is not complete binary tree!\n");
+
+	//拷贝
+	newroot = CopyBinTree(root);
+	PreOrder(newroot);
+	printf("\n");
+	InOrder(newroot);
+	printf("\n");
+	PostOrder(newroot);
+	printf("\n");
+
+	DestroyTree(&root);
+	DestroyTree(&newroot);
+}
---
+++ test0620/test0620/BinaryTree.h
@@ -0,0 +1,54 @@
+#pragma once
+
+
+//binary tree node data type
+typedef int BTNDataType;
+
+
+typedef struct BTNode
+{
+	struct BTNode* left;
+	struct BTNode* right;
+	BTNDataType data;
+}BTNode;
+
+// 创建
+BTNode* CreateBinTree(int array[], int size, int* index, int invaild);
+// 拷贝
+BTNode* CopyBinTree(BTNode* root);
+
+// 前序遍历
+void PreOrder(BTNode* root);
+// 中序遍历
+void InOrder(BTNode* root);
+// 后序遍历
+void PostOrder(BTNode* root);
+
+// 层序遍历
+void LevelOrder(BTNode* root);
+
+// 二叉树节点个数
+int GetNodeCount(BTNode* root);
+// 二叉树叶子节点个数
+int GetLeafNodeCount(BTNode* root);
+// 二叉树第k层节点个数
+int GetKLevelNodeCount(BTNode* root, unsigned int k);
+// 二叉树的高度
+int GetHight(BTNode* root);
+
+// 二叉树查找值为data的节点
+BTNode* Find(BTNode* root, BTNDataType data);
+
+// 二叉树销毁
+void DestroyTree(BTNode** root);
+
+
+// 判断二叉树是否是完全二叉树
+int BinaryTreeComplete(BTNode* root);
+
+
+
+
+
+//////////////////////////////////
+void TestBinTree();
