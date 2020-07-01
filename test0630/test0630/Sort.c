5df42c84b82cdcf14f77aa40715ca583b25e8cba
Straw-b <1609332043@qq.com>
2020-07-01 22:29:58 +0800

    希尔排序、选择排序、堆排序

---
+++ test0630/test0630/Sort.c
@@ -0,0 +1,95 @@
+#include "Sort.h"
+
+
+//打印
+void PrintArray(int array[], int size)
+{
+	for (int i = 0; i < size; ++i)
+		printf("%d ", array[i]);
+	printf("\n");
+
+}
+
+// 插入排序
+void InsertSort(int array[], int size)
+{
+	
+
+}
+
+
+// 希尔排序
+void ShellSort(int array[], int size)
+{
+	int gap = 1;
+	while (gap > 0)
+	{
+		// 注意：i从1开始--->原因
+		int key = array[i];
+		int end = i - gap;
+
+	}
+	
+}
+
+
+// 选择排序
+void SelectSort(int array[], int size)
+{
+	for (int i = 0; i < size - 1; ++i)
+	{
+
+
+	}
+
+}
+
+//向下调整
+void HeapAdjustDwon(int array[], int size, int parent)
+{
+	int child = parent * 2 + 1;
+
+	while (child < size);
+	{
+
+		if (array[child + 1] > array[child])
+			child += 1;
+	}
+
+
+
+}
+
+// 堆排序
+void HeapSort(int array[], int size)
+{
+	// 1.建堆
+	//
+	for (int root = (size - 2) / 2; root >= 0; root--)
+		HeapAdjustDwon(array, size, root);
+	
+	// 2.排序
+
+
+
+}
+
+
+
+
+
+///////////////////////////////////////////////////
+void TestSort()
+{
+	int array[] = { 4, 1, 7, 6, 3, 9, 5, 8, 0, 2 };
+	PrintArray(array, sizeof(array) / sizeof(array[0]));
+
+	InsertSort(array, sizeof(array) / sizeof(array[0]));
+
+	ShellSort();
+
+	SelectSort();
+
+	PrintArray(array, sizeof(array) / sizeof(array[0]));
+
+}
---
+++ test0630/test0630/Sort.h
@@ -0,0 +1,27 @@
+#pragma once
+
+
+//≤Â»Î≈≈–Ú
+void InsertSort(int array[], int size);
+
+// œ£∂˚≈≈–Ú
+void ShellSort(int array[], int size);
+
+// —°‘Ò≈≈–Ú
+void SelectSort(int array[], int size);
+
+// ∂—≈≈–Ú
+void HeapAdjustDwon(int array[], int size, int parent);
+void HeapSort(int array[], int size);
+
+
+
+
+
+
+
+
+
+
+/////////////////////////////////////////////////////////
+void TestSort();
